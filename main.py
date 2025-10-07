a7e1425384db3816435bf992a4ac975444c4cb1e
mpw2004 <2004mpw@gmail.com>
2025-10-07 10:09:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-## Given x (int) print a christmas tree of height x
 def print_christmas_tree(x):
-    for i in range(x):
-        print(" " * (x - i - 1) + "*" * (2 * i + 1))
+    for i in range(1, x + 1):
+        spaces = ' ' * (x - i)
+        stars = '*' * (2 * i - 1)
+        print(spaces + stars)
 
-## x0 = "apple", y0 = "adcsjncjsppaxjjnaxle" --> True if all characters of x0 are in y0 in order, y0 = "bsdpple" --> False, y0 = "paple" --> False
 def is_subsequence(x0, y0):
-    i, j = 0, 0
-    while i < len(x0) and j < len(y0):
-        if x0[i] == y0[j]:
-            i += 1
-        j += 1
-    return i == len(x0)
+    index = 0
+    for ch in y0:
+        if index < len(x0) and x0[index] == ch:
+            index += 1
+    return index == len(x0)
 
-# Example usage:
+# Example usage
 print_christmas_tree(10)
 print(is_subsequence("apple", "adcsjncjsppaxjjnaxle"))  # True
-print(is_subsequence("apple", "bsdpple"))  # False
-print(is_subsequence("apple", "paple"))  # False
+print(is_subsequence("apple", "bsdpple"))               # False
+print(is_subsequence("apple", "paple"))                 # False
