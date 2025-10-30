586d66c52bd7310e0a5861b38137d5e826920d35
mpw2004 <2004mpw@gmail.com>
2025-10-30 17:24:14 +0530

    update the lecture notes

--- Session_1/Lecture_notes/Intermediate_python/statistics_median.py
+++ Session_1/Lecture_notes/Intermediate_python/statistics_median.py
@@ -5,6 +5,7 @@ for i in range(len(data)):
     median = statistics.median(new_list)
     print(f"Median of {new_list} is {median}")
 
+#calculating the changing median as we add more data points
 import statistics
 data = [1, 3, 3, 6, 7, 8, 9]
 for i in range(len(data)):
